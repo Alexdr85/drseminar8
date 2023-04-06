cf507e76a53eb57f21a150d0a7b1ad503c70a036
Alexandr <dunkelpunk@gmail.com>
2023-04-06 21:00:36 +0300

    dr8

--- zadacha3/Program.cs
+++ zadacha3/Program.cs
@@ -1,2 +1,79 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить 
+//произведение двух матриц.
+//Например, даны 2 матрицы:
+//2 4 | 3 4
+//3 2 | 3 3
+//Результирующая матрица будет:
+//18 20
+//15 18
+
+
+
+void MultiplyMatrix(int[,] firstMartrix, int[,] secomdMartrix, int[,] resultMatrix)
+{
+  for (int i = 0; i < resultMatrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < resultMatrix.GetLength(1); j++)
+    {
+      int sum = 0;
+      for (int k = 0; k < firstMartrix.GetLength(1); k++)
+      {
+        sum += firstMartrix[i,k] * secomdMartrix[k,j];
+      }
+      resultMatrix[i,j] = sum;
+    }
+  }
+}
+
+int InputNumbers(string input)
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
+
+void CreateMatrix(int[,] matrix)
+{
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+      matrix[i, j] = new Random().Next(1,10);
+    }
+  }
+}
+
+void WriteMatrix (int[,] matrix)
+{
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+      Console.Write(matrix[i,j] + " ");
+    }
+    Console.WriteLine();
+  }
+}
+
+
+Console.WriteLine("Введите размеры матриц :");
+int m = InputNumbers("Введите число строк 1-й матрицы: ");
+int n = InputNumbers("Введите число столбцов 1-й матрицы (и строк 2-й): ");
+int p = InputNumbers("Введите число столбцов 2-й матрицы: ");
+
+
+int[,] firstMartrix = new int[m, n];
+CreateMatrix(firstMartrix);
+Console.WriteLine($"Первая матрица:");
+WriteMatrix(firstMartrix);
+
+int[,] secomdMartrix = new int[n, p];
+CreateMatrix(secomdMartrix);
+Console.WriteLine($"Вторая матрица:");
+WriteMatrix(secomdMartrix);
+
+int[,] resultMatrix = new int[m,p];
+
+MultiplyMatrix(firstMartrix, secomdMartrix, resultMatrix);
+Console.WriteLine($"Произведение первой и второй матриц:");
+WriteMatrix(resultMatrix);
